9c8606b935e7ccefb746a18fc1bd0482448875c3
paulden <paul.de-nonancourt@student.ecp.fr>
2017-03-05 20:38:41 +0100

    Fixed dumb mistake in slope calculation

--- lines_detection.py
+++ lines_detection.py
@@ -1,6 +1,6 @@
 from image import Image
 from functions import *
-from statistics import stdev, mean
+from statistics import stdev, mean, median
 from matplotlib import pyplot as plt
 import cv2
 import numpy as np
@@ -16,23 +16,22 @@ edges = chord_bm.edges_sobely()
 edges = threshold(edges, 127)
 
 lines = chord_bm.lines_hough_transform(edges, 50, 50)
-print(lines)
-coefficients = []
+size = len(lines)
+slopes = []
 
-for x in range(len(lines)):
+for x in range(size):
     for x1, y1, x2, y2 in lines[x]:
-        coefficients.append(abs((x1-y1)/(x2-y2)))
+        slopes.append(abs((y2 - y1) / (x2 - x1)))
 
-print(coefficients)
-coefficients = sorted(set(coefficients))
-print(coefficients)
+mean_slope = mean(slopes)
+median_slope = median(slopes)
+std_slope = stdev(slopes)
 
-for x in range(len(lines)):
+for x in range(size):
     for x1, y1, x2, y2 in lines[x]:
-        if abs((x1-y1)/(x2-y2)) == 12.75:
-            print(lines)
+        if abs(abs((y2-y1)/(x2-x1)) - mean_slope) < std_slope/2:
             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 
-# plt.imshow(img)
-# plt.show()
+plt.imshow(img)
+plt.show()
